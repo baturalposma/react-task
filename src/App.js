7dd697464abae51d8da79080bc55eea8bf2d0575
baturalposma <baturalposmaa@gmail.com>
2023-01-24 00:58:37 +0300

    Change files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import React from 'react';
 import {
-  BrowserRouter as Router,
-  Routes,
+  Router,
   Route,
-  Navigate
-} from "react-router-dom";
-
+  Link
+} from 'react-router-dom'
 import UTMPage from './UTMPage';
 import LeadForm from './LeadForm';
-import ThankYouPage from './ThankYouPage';
+import ThankYou from './ThankYouPage';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the landing page of your application.</p>
+    </div>
+  );
+};
 
 function App() {
-  ReactDOM.render (
+  return (
     <Router>
-      <Routes>
-        <Route path="/utm" element={<UTMPage/>} />
-        <Route path="/lead-form" element={<LeadForm/>} />
-        <Route path="/thank-you" element={<ThankYouPage/>} />
-        <Navigate from="/" to="/utm" />
-      </Routes>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/utm">UTM</Link>
+          <Link to="/leadform">Lead Form</Link>
+          <Link to="/thank-you">Thank You</Link>
+        </nav>
+        <Route path="/" exact component={Home} />
+        <Route path="/utm" component={UTMPage} />
+        <Route path="/leadform" component={LeadForm} />
+        <Route path="/thank-you" component={ThankYou} />
+      </div>
     </Router>
   );
 }
--- src/LeadForm.js
+++ src/LeadForm.js
@@ -1,45 +1,82 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Formik, Form, Field } from 'formik';
-import axios from 'axios';
+import React, { useEffect } from 'react';
+import { Formik, Form, Field, useFormik } from 'formik';
+import intlTelInput from 'intl-tel-input';
+import { useNavigate } from 'react-router-dom'
 
 const LeadForm = () => {
-const navigate = useNavigate();
-  let history = navigate();
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const utmSource = urlParams.get('utm_source');
+    const utmMedium = urlParams.get('utm_medium');
+    const utmTerm = urlParams.get('utm_term');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
+    localStorage.setItem('utmSource', utmSource);
+    localStorage.setItem('utmMedium', utmMedium);
+    localStorage.setItem('utmTerm', utmTerm);
+  }, []);
 
-    axios.post('/api/submit-form', formData)
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.error(error);
-    });
-
-    history.push("/thank-you");
-  };
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      fullname: '',
+      email: '',
+      phone: ''
+    },
+    validate: values => {
+      const errors = {};
+      if (!values.fullname) {
+        errors.fullname = 'Full name is required';
+      }
+      if (!values.email) {
+        errors.email = 'Email is required';
+      } else if (!emailRegex.test(values.email)) {
+        errors.email = 'Invalid email address';
+      }
+      if (!values.phone) {
+        errors.phone = 'Phone number is required';
+      } else if (!intlTelInput.validate(values.phone)) {
+        errors.phone = 'Invalid phone number';
+      }
+      return errors;
+    },
+    
+    onSubmit: (values, { setSubmitting }) => {
+      // Add logic to submit the form
+      // and navigate to the thank you page
+      setTimeout(() => {
+        setSubmitting(false);
+        navigate('/thank-you')
+      }, 2000);
+    }
+  });
 
   return (
-    <div className="app">
-        <Formik
-            initialValues={{ fullName: '', email: '', phoneNumber: '' }}
-            onSubmit={handleSubmit}
-            >
-            {({ isSubmitting }) => (
-                <Form>
-                <Field type="text" name="fullName" placeholder="Full Name" />
-                <Field type="email" name="email" placeholder="Email Address" />
-                <Field type="tel" name="phoneNumber" placeholder="Phone Number" />
-                <button type="submit" disabled={isSubmitting}>
-                    Submit
-                </button>
-                </Form>
-            )}
-        </Formik>
-    </div>
+    <Formik {...formik}>
+      <Form>
+        <div>
+          <Field name="fullname" placeholder="Full Name" />
+          {formik.errors.fullname && formik.touched.fullname && (
+            <div>{formik.errors.fullname}</div>
+          )}
+        </div>
+        <div>
+          <Field name="email" placeholder="Email" type="email" />
+          {formik.errors.email && formik.touched.email && (
+            <div>{formik.errors.email}</div>
+          )}
+        </div>
+        <div>
+          <Field name="phone" placeholder="Phone" type="tel" />
+          {formik.errors.phone && formik.touched.phone && (
+            <div>{formik.errors.phone}</div>
+          )}
+        </div>
+        <button type="submit" disabled={formik.isSubmitting}>
+          Submit
+        </button>
+      </Form>
+    </Formik>
   );
 };
 
